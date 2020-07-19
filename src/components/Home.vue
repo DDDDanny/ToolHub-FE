25d834a45ea4cde55aafde0e7fb3fcc49e584034
DDDDanny <707956456@qq.com>
2020-07-19 21:16:12 +0800

    feat: 修改Home样式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@
                         <v-icon>mdi-home</v-icon>
                     </v-list-item-action>
                     <v-list-item-content>
-                        <v-list-item-title>Home</v-list-item-title>
+                        <v-list-item-title class="title_temp">Home</v-list-item-title>
                     </v-list-item-content>
                 </v-list-item>
                 <v-list-group :prepend-icon="item.iconName" no-action v-for="item in menuList" :key="item.nodeName">
