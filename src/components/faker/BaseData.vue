e008288b1bb1fd5ae14b17026b632b8eebaa0c85
DDDDanny <707956456@qq.com>
2020-07-21 23:26:11 +0800

    feat: 完成FakerData基础页面UI结构

---
+++ src/components/faker/BaseData.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <v-breadcrumbs :items="breadItems" dark></v-breadcrumbs>
+        <v-container>
+            <v-row>
+                <v-col cols="6" md="2">
+                    <v-card class="pa-5" dark elevation="10" height="750px">
+                        world
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-row no-gutters>
+                        <v-col cols="12" md="12">
+                            <!-- 卡片视图 -->
+                            <v-card dark elevation="10" class="pa-5" height="320px">
+                                hello
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                    <v-row style="margin-top: 20px">
+                        <v-col cols="12" md="12">
+                            <!-- 卡片视图 -->
+                            <v-card dark elevation="10" class="pa-5" height="400px">
+                                hello
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                breadItems: [
+                    { text: 'Home', disabled: false, to: '/' },
+                    { text: 'FakerData', disabled: false },
+                    { text: 'Base', disabled: false }
+                ],
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from "@/components/Home";
 import AboutMe from "@/components/about/AboutMe"
 import Welcome from "@/components/Welcome"
 import AboutToolHub from "@/components/about/AboutToolHub"
+import BaseData from "@/components/faker/BaseData"
 
 Vue.use(VueRouter);
 
@@ -11,9 +12,10 @@ Vue.use(VueRouter);
   {
     path: '/', name: 'Home', component: Home, redirect: '/welcome',
     children: [
-       {path: '/welcome',  component: Welcome },
-       {path: '/about/me',  component: AboutMe },
-       {path: '/about/toolHub',  component: AboutToolHub },
+       { path: '/welcome',  component: Welcome },
+       { path: '/about/me',  component: AboutMe },
+       { path: '/about/toolHub',  component: AboutToolHub },
+       { path: '/faker/base',  component: BaseData },
     ]
   },
 ];
