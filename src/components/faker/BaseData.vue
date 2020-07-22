a9254fe2a08c6bdc810c0b50f9a219d42ff5be9d
DDDDanny <707956456@qq.com>
2020-07-22 23:54:22 +0800

    feat: 完善FakerData基础页面UI结构

--- src/components/faker/BaseData.vue
+++ src/components/faker/BaseData.vue
@@ -4,24 +4,51 @@
         <v-container>
             <v-row>
                 <v-col cols="6" md="2">
-                    <v-card class="pa-5" dark elevation="10" height="750px">
-                        world
+                    <v-card  dark elevation="10" height="750px">
+
+                        <v-card-title style="font-size: 15px">
+                            <v-icon>mdi-format-list-bulleted-type</v-icon>
+                            <span style="margin-left: 10px">类别</span>
+                        </v-card-title>
+                        <hr>
+                        <v-list nav>
+                            <v-list-item-group v-model="item" color="primary">
+                                <v-list-item v-for="(item, i) in items" :key="i">
+                                    <v-list-item-content>
+                                        <v-list-item-title v-html="item.title"></v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list-item-group>
+                        </v-list>
                     </v-card>
                 </v-col>
                 <v-col>
                     <v-row no-gutters>
                         <v-col cols="12" md="12">
                             <!-- 卡片视图 -->
-                            <v-card dark elevation="10" class="pa-5" height="320px">
-                                hello
+                            <v-card dark elevation="10" height="320px">
+                                <v-card-title style="font-size: 15px">
+                                    <v-icon>mdi-basket-fill</v-icon>
+                                    <span style="margin-left: 10px">参数&条件</span>
+                                </v-card-title>
+                                <hr>
                             </v-card>
                         </v-col>
                     </v-row>
                     <v-row style="margin-top: 20px">
                         <v-col cols="12" md="12">
                             <!-- 卡片视图 -->
-                            <v-card dark elevation="10" class="pa-5" height="400px">
-                                hello
+                            <v-card dark elevation="10" height="400px">
+                                <v-card-title style="font-size: 15px">
+                                    <v-icon>mdi-duck</v-icon>
+                                    <span style="margin-left: 10px">结果</span>
+                                </v-card-title>
+                                <hr>
+                                <v-col cols="12" md="12">
+                                    <v-textarea label="Result" auto-grow outlined rows="1" row-height="12" dark
+                                                height="300px" readonly class="pa-2" :value="result" filled></v-textarea>
+                                </v-col>
+
                             </v-card>
                         </v-col>
                     </v-row>
@@ -40,6 +67,18 @@
                     { text: 'FakerData', disabled: false },
                     { text: 'Base', disabled: false }
                 ],
+                items: [
+                    {
+                        title: 'Brunch this weekend?',
+                    },
+                    {
+                        title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+                    },
+                    {
+                        title: 'Oui oui',
+                    },
+                ],
+                result: '这是Faker Data结果信息！'
             }
         }
     }
