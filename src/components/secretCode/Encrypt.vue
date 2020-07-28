28735b973f6e567c9d3b82e21710aead3c7086d4
DDDDanny <707956456@qq.com>
2020-07-28 23:45:19 +0800

    feat: 完善加密UI结构，完成整体布局

--- src/components/secretCode/Encrypt.vue
+++ src/components/secretCode/Encrypt.vue
@@ -10,6 +10,28 @@
                         <span style="margin-left: 10px">待加密文 (PlainText)</span>
                     </v-card-title>
                     <hr>
+                    <v-form class="mt-2">
+                        <v-container fluid>
+                            <v-radio-group row dark label="加密类型：">
+                                <v-radio label="Base64" value="radio-1"></v-radio>
+                                <v-radio label="MD5" value="radio-2"></v-radio>
+                            </v-radio-group>
+                            <v-col cols="24" md="24">
+                                <v-row justify="center">
+                                    <v-col cols="12">
+                                        <v-textarea label="明文（PlainText）" height="100" no-resize outlined rows="3" row-height="10"></v-textarea>
+                                    </v-col>
+                                </v-row>
+                            </v-col>
+                            <v-col cols="24" md="24">
+                                <v-row justify="end">
+                                    <v-col cols="12" md="2">
+                                        <v-btn color="blue" class="mr-4" large width="200px">加密</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-col>
+                        </v-container>
+                    </v-form>
                 </v-card>
             </v-col>
         </v-row>
@@ -21,6 +43,13 @@
                         <span style="margin-left: 10px">密文 (CipherText)</span>
                     </v-card-title>
                     <hr>
+                    <v-col cols="24" md="20" class="ma-4">
+                        <v-row justify="center">
+                            <v-col cols="12">
+                                <v-textarea label="密文（CipherText）" height="200" no-resize outlined rows="6" row-height="10"></v-textarea>
+                            </v-col>
+                        </v-row>
+                    </v-col>
                 </v-card>
             </v-col>
         </v-row>
