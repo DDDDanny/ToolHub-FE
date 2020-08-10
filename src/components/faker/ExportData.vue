4f7bfca9904775d9802941ba2b59e825f2f77127
DDDDanny <707956456@qq.com>
2020-08-10 23:05:55 +0800

    feat: 完成数据导出页面基础交互

--- src/components/faker/ExportData.vue
+++ src/components/faker/ExportData.vue
@@ -11,25 +11,25 @@
                     <hr>
                     <v-row align="center" class="ma-4">
                         <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="姓名 (Name)" color="info" value="name"></v-checkbox>
+                            <v-checkbox label="姓名 (Name)" color="info" v-model="checkboxModel.name"></v-checkbox>
                         </v-col>
                         <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="性别 (Gender)" color="info" value="gender"></v-checkbox>
+                            <v-checkbox label="性别 (Gender)" color="info" v-model="checkboxModel.gender"></v-checkbox>
                         </v-col>
                         <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="电话 (Phone)" color="info" value="phone"></v-checkbox>
+                            <v-checkbox label="电话 (Phone)" color="info" v-model="checkboxModel.phone"></v-checkbox>
                         </v-col>
                         <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="地址 (Address)" color="info" value="address"></v-checkbox>
+                            <v-checkbox label="地址 (Address)" color="info" v-model="checkboxModel.addr"></v-checkbox>
                         </v-col>
                         <v-col class="d-flex" cols="12" md="2">更多(More)...</v-col>
                         <v-col class="d-flex" cols="12" md="6">
-                            <v-text-field label="数据生成数量 (Number)" required></v-text-field>
+                            <v-text-field label="数据生成数量 (Number)" required v-model="dataNum"></v-text-field>
                         </v-col>
                         <v-col cols="24" md="12">
                             <v-row justify="end" class="mt-2">
                                 <v-col cols="12" md="1">
-                                    <v-btn dark class="mr-1" color="success" large elevation="12">预览</v-btn>
+                                    <v-btn dark class="mr-1" color="success" large elevation="12" @click="previewTable">预览</v-btn>
                                 </v-col>
                                 <v-col cols="12" md="2" class="ml-4">
                                     <v-btn dark class="mr-4" large width="200px" color="blue" elevation="12">导出数据</v-btn>
@@ -46,25 +46,24 @@
                         <span style="margin-left: 10px">预览 (Preview)</span>
                     </v-card-title>
                     <hr>
-                    <v-simple-table dark class="ma-4">
-                        <template v-slot:default>
+                    <v-row v-if="emptyFlag" justify="center" style="margin-top: 100px">
+                        <v-alert color="blue" dark elevation="12" width="450px" icon="mdi-google-downasaur" style="align-items: center" border="left">
+                            请选择参数后，再进行预览。不选必然为空喽~
+                        </v-alert>
+                    </v-row>
+                    <v-simple-table dark class="ma-4" v-else>
+                        <template>
                             <thead>
-                            <tr>
-                                <th class="text-left" style="font-size: 18px">姓名</th>
-                                <th class="text-left" style="font-size: 18px">年龄</th>
-                                <th class="text-left" style="font-size: 18px">地址</th>
-                            </tr>
+                                <tr>
+                                    <th class="text-left" style="font-size: 18px" v-for="item in this.tableHeader" :key="item">{{ item }}</th>
+                                </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>张三</td>
-                                    <td>12</td>
-                                    <td>成都市高新区</td>
-                                </tr>
-                                <tr>
-                                    <td>李四</td>
-                                    <td>18</td>
-                                    <td>成都市天府新区</td>
+                                <tr v-for="(item, i) in this.tableExpData" :key="i">
+                                    <td v-if="tableHeader.includes('姓名')">{{ item.name }}</td>
+                                    <td v-if="tableHeader.includes('性别')">{{ item.gender }}</td>
+                                    <td v-if="tableHeader.includes('电话')">{{ item.phone }}</td>
+                                    <td v-if="tableHeader.includes('地址')">{{ item.addr }}</td>
                                 </tr>
                             </tbody>
                         </template>
@@ -72,7 +71,6 @@
                 </v-card>
             </v-col>
         </v-row>
-
     </div>
 </template>
 
@@ -85,10 +83,39 @@
                     { text: 'FakerData', disabled: false },
                     { text: 'ExportData', disabled: false }
                 ],
-                items: ['']
+                checkboxModel: {
+                    name: false, gender: false,
+                    phone: false, addr: false
+                },
+                tableExpData: [{}, {}, {}],
+                tableHeader: ['姓名', '性别', '电话', '地址'],
+                emptyFlag: false,
+                dataNum: ''
             }
         },
-
+        methods: {
+            previewTable() {
+                this.tableHeader = []
+                if (this.checkboxModel.name === true) {
+                    this.tableHeader.push('姓名')
+                    for (let i = 0; i < this.tableExpData.length; i++ ) this.tableExpData[i].name = '学生' + i
+                }
+                if (this.checkboxModel.gender === true) {
+                    this.tableHeader.push('性别')
+                    for (let i = 0; i < this.tableExpData.length; i++ ) this.tableExpData[i].gender = '性别' + i
+                }
+                if (this.checkboxModel.phone === true) {
+                    this.tableHeader.push('电话')
+                    for (let i = 0; i < this.tableExpData.length; i++ ) this.tableExpData[i].phone = '电话' + i
+                }
+                if (this.checkboxModel.addr === true) {
+                    this.tableHeader.push('地址')
+                    for (let i = 0; i < this.tableExpData.length; i++ ) this.tableExpData[i].addr = '地址' + i
+                }
+                if (this.tableHeader.length === 0) this.emptyFlag = true
+                console.log(this.tableExpData)
+            }
+        }
     }
 </script>
 
