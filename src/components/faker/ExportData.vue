5d09c0ac49cbf7795ad208dffb5d8064f36d3613
DDDDanny <707956456@qq.com>
2020-08-12 20:30:48 +0800

    feat: 新增导出参数表单数据校验

--- src/components/faker/ExportData.vue
+++ src/components/faker/ExportData.vue
@@ -9,34 +9,36 @@
                         <span style="margin-left: 10px">所需参数 (Parameters)</span>
                     </v-card-title>
                     <hr>
-                    <v-row align="center" class="ma-4">
-                        <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="姓名 (Name)" color="info" v-model="checkboxModel.name"></v-checkbox>
-                        </v-col>
-                        <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="性别 (Gender)" color="info" v-model="checkboxModel.gender"></v-checkbox>
-                        </v-col>
-                        <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="电话 (Phone)" color="info" v-model="checkboxModel.phone"></v-checkbox>
-                        </v-col>
-                        <v-col class="d-flex" cols="12" md="2">
-                            <v-checkbox label="地址 (Address)" color="info" v-model="checkboxModel.addr"></v-checkbox>
-                        </v-col>
-                        <v-col class="d-flex" cols="12" md="2">更多(More)...</v-col>
-                        <v-col class="d-flex" cols="12" md="6">
-                            <v-text-field label="数据生成数量 (Number)" required v-model="dataNum"></v-text-field>
-                        </v-col>
-                        <v-col cols="24" md="12">
-                            <v-row justify="end" class="mt-2">
-                                <v-col cols="12" md="1">
-                                    <v-btn dark class="mr-1" color="success" large elevation="12" @click="previewTable">预览</v-btn>
-                                </v-col>
-                                <v-col cols="12" md="2" class="ml-4">
-                                    <v-btn dark class="mr-4" large width="200px" color="blue" elevation="12" @click="exportData">导出数据</v-btn>
-                                </v-col>
-                            </v-row>
-                        </v-col>
-                    </v-row>
+                    <v-form ref="form">
+                        <v-row align="center" class="ma-4">
+                            <v-col class="d-flex" cols="12" md="2">
+                                <v-checkbox label="姓名 (Name)" color="info" v-model="checkboxModel.name"></v-checkbox>
+                            </v-col>
+                            <v-col class="d-flex" cols="12" md="2">
+                                <v-checkbox label="性别 (Gender)" color="info" v-model="checkboxModel.gender"></v-checkbox>
+                            </v-col>
+                            <v-col class="d-flex" cols="12" md="2">
+                                <v-checkbox label="电话 (Phone)" color="info" v-model="checkboxModel.phone"></v-checkbox>
+                            </v-col>
+                            <v-col class="d-flex" cols="12" md="2">
+                                <v-checkbox label="地址 (Address)" color="info" v-model="checkboxModel.addr"></v-checkbox>
+                            </v-col>
+                            <v-col class="d-flex" cols="12" md="2">更多(More)...</v-col>
+                            <v-col class="d-flex" cols="12" md="6">
+                                <v-text-field label="数据生成数量 (Quantity)" required v-model="dataNum" :rules="dataNumRules"></v-text-field>
+                            </v-col>
+                            <v-col cols="24" md="12">
+                                <v-row justify="end" class="mt-2">
+                                    <v-col cols="12" md="1">
+                                        <v-btn dark class="mr-1" color="success" large elevation="12" @click="previewTable">预览</v-btn>
+                                    </v-col>
+                                    <v-col cols="12" md="2" class="ml-4">
+                                        <v-btn dark class="mr-4" large width="200px" color="blue" elevation="12" @click="exportData">导出数据</v-btn>
+                                    </v-col>
+                                </v-row>
+                            </v-col>
+                        </v-row>
+                    </v-form>
                 </v-card>
             </v-col>
             <v-col cols="12" md="12">
@@ -85,12 +87,16 @@
                 ],
                 checkboxModel: {
                     name: false, gender: false,
-                    phone: false, addr: false
+                    phone: false, addr: false,
                 },
                 tableExpData: [{}, {}, {}],
                 tableHeader: ['姓名', '性别', '电话', '地址'],
                 emptyFlag: false,
-                dataNum: ''
+                dataNum: '',
+                dataNumRules: [
+                    v => !!v || '数量不能为空 (Quantity is required)',
+                    v => /^[1-9]\d*|0$/.test(v) || '只能输入数字 (Only Enter Numbers)'
+                ]
             }
         },
         methods: {
@@ -115,6 +121,8 @@
                 this.emptyFlag = this.tableHeader.length === 0;
             },
             exportData() {
+                const jud = this.$refs.form.validate()
+                if (!jud) return
                 const {data: res} = this.$http.get('/faker/export',{
                     responseType: 'blob',
                     params: {paramsList: this.tableHeader.toString(), dataNum: this.dataNum}
