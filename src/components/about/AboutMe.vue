25eb3cb6fd4495a93c0219a647d8a4e512452036
DDDDanny <707956456@qq.com>
2020-08-13 22:34:38 +0800

    feat: 完成AboutMe基础UI结构

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -2,9 +2,14 @@
     <div>
         <v-breadcrumbs :items="breadItems" dark></v-breadcrumbs>
         <!-- 卡片视图 -->
-        <v-card dark elevation="10">
-            hello
-        </v-card>
+        <v-row justify="center">
+            <v-card dark elevation="8" height="250" width="250" class="avatar-img" style="border-radius: 50%;top: 80px;z-index: 1;border: 5px solid #eee;">
+                <v-img dark src="../../static/avatar.png" style="width: 100%;height: 100%;border-radius: 50%;"></v-img>
+            </v-card>
+        </v-row>
+        <v-row justify="center">
+            <v-card dark elevation="10" height="500" width="1000" style="top: -50px;"></v-card>
+        </v-row>
     </div>
 </template>
 
