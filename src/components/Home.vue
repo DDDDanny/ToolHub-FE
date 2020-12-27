d8efba99e99df46c99f3f8cd015b34fbb5504470
DDDDanny <707956456@qq.com>
2020-12-27 22:47:15 +0800

    style: 优化代码格式

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,78 +1,89 @@
 <template>
-    <v-app id="inspire">
-        <v-navigation-drawer v-model="drawer" app clipped>
-            <v-list rounded>
-                <v-list-item link to="/welcome">
-                    <v-list-item-action>
-                        <v-icon>mdi-home</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title class="">Home</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-group :prepend-icon="iconName" no-action v-for="{children, iconName, nodeName} in menuList" :key="nodeName">
-                    <template v-slot:activator>
-                        <v-list-item-title class="">{{ nodeName }}</v-list-item-title>
-                    </template>
-                    <v-list-item v-for="{iconName, nodeName, path} in (children)" :key="nodeName" link :to="path">
-                        <v-list-item-title v-text="nodeName" class="title_temp"></v-list-item-title>
-                        <v-list-item-icon>
-                            <v-icon v-text="iconName"></v-icon>
-                        </v-list-item-icon>
-                    </v-list-item>
-                </v-list-group>
-            </v-list>
-        </v-navigation-drawer>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer" app clipped>
+			<v-list rounded>
+				<v-list-item link to="/welcome">
+					<v-list-item-action>
+						<v-icon>mdi-home</v-icon>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title class="">Home</v-list-item-title>
+					</v-list-item-content>
+				</v-list-item>
+				<v-list-group
+					:prepend-icon="iconName"
+					no-action
+					v-for="{ children, iconName, nodeName } in menuList"
+					:key="nodeName"
+				>
+					<template v-slot:activator>
+						<v-list-item-title class="">{{ nodeName }}</v-list-item-title>
+					</template>
+					<v-list-item
+						v-for="{ iconName, nodeName, path } in children"
+						:key="nodeName"
+						link
+						:to="path"
+					>
+						<v-list-item-title v-text="nodeName" class="title_temp"></v-list-item-title>
+						<v-list-item-icon>
+							<v-icon v-text="iconName"></v-icon>
+						</v-list-item-icon>
+					</v-list-item>
+				</v-list-group>
+			</v-list>
+		</v-navigation-drawer>
 
-        <v-app-bar app clipped-left>
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-            <!-- <img src="../assets/logo.png" style="width: 90px; margin-left: 25px;"/> -->
-            <v-toolbar-title>ToolHub</v-toolbar-title>
-        </v-app-bar>
+		<v-app-bar app clipped-left>
+			<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+			<!-- <img src="../assets/logo.png" style="width: 90px; margin-left: 25px;"/> -->
+			<v-toolbar-title>ToolHub</v-toolbar-title>
+		</v-app-bar>
 
-        <v-main>
-            <!-- 路由占位符 -->
-            <router-view></router-view>
-        </v-main>
+		<v-main>
+			<!-- 路由占位符 -->
+			<router-view></router-view>
+		</v-main>
 
-        <v-footer app>
-            <span style="font-size: x-small">&copy; {{ new Date().getFullYear() }} By DDDDanny
-                <v-icon size="large">mdi-zodiac-aquarius</v-icon>
-            </span>
-        </v-footer>
-    </v-app>
+		<v-footer app>
+			<span style="font-size: x-small"
+				>&copy; {{ new Date().getFullYear() }} By DDDDanny
+				<v-icon size="large">mdi-zodiac-aquarius</v-icon>
+			</span>
+		</v-footer>
+	</v-app>
 </template>
 
 <script>
-    export default {
-        props: {
-            source: String,
-        },
-        data() {
-            return {
-                drawer: null,
-                menuList: [],
-            }
-        },
-        created() {
-            this.getMenuList()
-            this.$vuetify.theme.dark = true
-        },
-        methods: {
-            async getMenuList() {
-                const {data: res} = await this.$http.get('menu')
-                if (res.meta.status !== 200) {
-                    return
-                }
-                this.menuList = res.data
-                console.log(this.menuList)
-            }
-        }
-    }
+	export default {
+		props: {
+			source: String,
+		},
+		data() {
+			return {
+				drawer: null,
+				menuList: [],
+			};
+		},
+		created() {
+			this.getMenuList();
+			this.$vuetify.theme.dark = true;
+		},
+		methods: {
+			async getMenuList() {
+				const { data: res } = await this.$http.get("menu");
+				if (res.meta.status !== 200) {
+					return;
+				}
+				this.menuList = res.data;
+				console.log(this.menuList);
+			},
+		},
+	};
 </script>
 
 <style lang="less">
-    .title_temp{
-        font-size: 14px !important;
-    }
+	.title_temp {
+		font-size: 14px !important;
+	}
 </style>
--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,66 +1,71 @@
 <template>
-    <div>
-        <v-row class="mb-3">
-            <v-col :span="24" md="24">
-                <v-card-title class="ml-3" style="font-size: 35px">ToolHub Summary：</v-card-title>
-            </v-col>
-        </v-row>
-        <v-row v-for="item in welcomeInfo" :key="item">
-            <v-col :span="24" md="24">
-               <v-card dark elevation="15" style="height:90px;">
-                   <v-row>
-                       <v-col :span="24" :md="1" class="ml-6">
-                            <img :src="item.ImgPath" style="height: 45%;"/>
-                       </v-col>
-                       <v-col :span="24" :md="2">
-                           <v-card-title style="font-size: 25px;">{{ item.Title }}</v-card-title>
-                       </v-col>
-                       <v-col :span="24">
-                           <v-card-title style="size:80px">{{ item.Desc }}</v-card-title>
-                       </v-col>
-                   </v-row>
-                </v-card> 
-            </v-col>
-        </v-row>
-    </div>
+	<div>
+		<v-row class="mb-3">
+			<v-col :span="24" md="24">
+				<v-card-title class="ml-3" style="font-size: 35px"
+					>ToolHub Summary：</v-card-title
+				>
+			</v-col>
+		</v-row>
+		<v-row v-for="item in welcomeInfo" :key="item">
+			<v-col :span="24" md="24">
+				<v-card dark elevation="15" style="height:90px;">
+					<v-row>
+						<v-col :span="24" :md="1" class="ml-6">
+							<img :src="item.ImgPath" style="height: 45%;" />
+						</v-col>
+						<v-col :span="24" :md="2">
+							<v-card-title style="font-size: 25px;">{{
+								item.Title
+							}}</v-card-title>
+						</v-col>
+						<v-col :span="24">
+							<v-card-title style="size:80px">{{ item.Desc }}</v-card-title>
+						</v-col>
+					</v-row>
+				</v-card>
+			</v-col>
+		</v-row>
+	</div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                welcomeInfo: [
-                    {
-                        Title: 'FakerData',
-                        Desc: '基于Python Faker库生成随机数据，实现基础数据生成及导出',
-                        ImgPath: require('../assets/stickers_033.png')
-                    },
-                    {
-                        Title: 'SecretCode',
-                        Desc: '用于处理数据的加密与解密: 支持MD5、hmac、sha1加密和Base64加解密',
-                        ImgPath: require('../assets/stickers_027.png')
-                    },
-                    {
-                        Title: 'Calculate',
-                        Desc: '用于计算数据: 支持计算商品在打折后的实付价格',
-                        ImgPath: require('../assets/stickers_005.png')
-                    },
-                    {
-                        Title: 'FormatData',
-                        Desc: '用于格式化数据: 支持格式化Json数据',
-                        ImgPath: require('../assets/stickers_030.png')
-                    }
-                ]
-            }
-        }
-    }
+	export default {
+		data() {
+			return {
+				welcomeInfo: [
+					{
+						Title: "FakerData",
+						Desc: "基于Python Faker库生成随机数据，实现基础数据生成及导出",
+						ImgPath: require("../assets/stickers_033.png"),
+					},
+					{
+						Title: "SecretCode",
+						Desc:
+							"用于处理数据的加密与解密: 支持MD5、hmac、sha1加密和Base64加解密",
+						ImgPath: require("../assets/stickers_027.png"),
+					},
+					{
+						Title: "Calculate",
+						Desc: "用于计算数据: 支持计算商品在打折后的实付价格",
+						ImgPath: require("../assets/stickers_005.png"),
+					},
+					{
+						Title: "FormatData",
+						Desc: "用于格式化数据: 支持格式化Json数据",
+						ImgPath: require("../assets/stickers_030.png"),
+					},
+				],
+			};
+		},
+	};
 </script>
 
 <style scoped lang="less">
-    .more-info {
-        font-size: 50px;
-        margin-left: 85px;
-        position: absolute;
-        top: 160px;
-    }
+	.more-info {
+		font-size: 50px;
+		margin-left: 85px;
+		position: absolute;
+		top: 160px;
+	}
 </style>
